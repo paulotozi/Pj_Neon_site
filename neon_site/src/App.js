82ea6c6f47b5180adc1cdd2426b76abb53bd2b0d
paulotozi <paulotozi1512@gmail.com>
2023-11-14 14:58:50 -0300

    Adicionando arquivos do projeto

---
+++ neon_site/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import image from "./image/image.png";
+import logo from "./image/logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div className="grad"></div>
+
+    </div>
+  );
+}
+
+export default App;
